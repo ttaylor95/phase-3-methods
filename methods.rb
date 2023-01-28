792e98b00433193728ac204bb40a568334430ffd
Teanna <teannamtaylor95@gmail.com>
2023-01-28 11:37:14 -0800

    commit tree abf9e6e012ca2de46875c3120cbfcced1bbe12cb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+  print "Hello, programmer!"
+end
+
+def greet(name ="Naureen")
+    print "Hello, #{name}!"
+end 
+
+def greet_with_default(name = "programmer")
+    print "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+   if num.is_a?(Integer)
+    num/2
+   else
+    nil
+   end
+end
